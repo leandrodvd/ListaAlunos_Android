d70a595a4cea8dd85ae80c3d8d9c2b0278ffdb83
Leandro <leandrodvd@gmail.com>
2016-01-06 19:37:25 -0200

    initial commig

---
+++ app/src/main/java/com/example/caelum/listaalunos/ListaAluno.java
@@ -0,0 +1,79 @@
+package com.example.caelum.listaalunos;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class ListaAluno extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_aluno);
+
+        String[] alunos ={"Clarck Kent","Peter Parker","Bruce Wayne"};
+
+        ListView lista = (ListView) this.findViewById(R.id.lista);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,alunos);
+        lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ListaAluno.this,"Item.Clicado" + position,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                String aluno = (String) parent.getItemAtPosition(position);
+
+                Toast.makeText(ListaAluno.this,aluno,Toast.LENGTH_LONG).show();
+
+                return true; //true or false dependendo se quiser mostrar
+            }
+        });
+
+        Button inserir  = (Button)findViewById(R.id.lista_alunos_floating_button);
+        inserir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ListaAluno.this,"Botão Clicado",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_lista_aluno, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
